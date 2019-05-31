e21f2ab79b1ba343f52378203f3d71e47ea2edfc
elnino228 <daodat228@gmail.com>
2019-05-31 17:45:33 +0700

    first

---
+++ src/KhaiBaoBien.java
@@ -0,0 +1,16 @@
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int i=10;
+        float f=10.2f;
+        double d=12.1;
+        char c='a';
+        boolean b=true;
+        String str="hello";
+        System.out.println(i);
+        System.out.println(f);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(str);
+    }
+}
